5280a35455d7fec0fe15d863ef66ae6964fffb19
McKenzie Quattlebaum <mckenziequattlebaum@McKenzies-MacBook-Pro.local>
2019-04-13 12:35:27 -0400

    customer view

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,11 +4,11 @@ var console_table = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 8889,
 
   user: "root",
 
-  password: "",
+  password: "root",
   database: "Bamazon"
 });
 
