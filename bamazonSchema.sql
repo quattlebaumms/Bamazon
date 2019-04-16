2acb83a20baec502b8a3cb1217542fcc4b1e87b3
McKenzie Quattlebaum <mckenziequattlebaum@McKenzies-MacBook-Pro.local>
2019-04-15 22:49:10 -0400

    final commit

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -13,3 +13,32 @@ CREATE TABLE products(
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("paper towels","home", 5.00, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("spark plug","auto", 12.50, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("instant ramen","grocery", 1.00, 1000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("toothbrush","personal care", 3.00, 250);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("toilet paper","personal care", 8.00, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("copy paper","office", 11.00, 300);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("ballpoint pen","office", 1.00, 2500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("50pk paper plates","home", 4.50, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("wiper fluid","auto", 8.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("deoderant","personal care", 4.50, 30);
+
+SELECT * FROM products;
